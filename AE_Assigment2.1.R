bf2a9d9cf393bbf90985e692d73b4f7b84f0e259
AzmyUSF <azmy@usf.edu>
2024-09-08 20:27:57 -0400

    Add files via upload

---
+++ AE_Assigment2.1.R
@@ -0,0 +1,10 @@
+# Assignment 2 (Corrected)
+assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+
+# Function to calculate the mean
+myMean <- function(data) { 
+  return(sum(data) / length(data))
+}
+
+# Using the function
+myMean(assignment2)
